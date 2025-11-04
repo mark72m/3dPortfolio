99868f4dbb067c4ef151af5b72e990785b4c27a9
markm <markmutisya72@gmail.com>
2025-11-03 23:40:14 -0800

    EDDY

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { words } from '../constants/index.js'
+import Button from '../components/Button.jsx'
+import HeroExperience from '../components/HeroModels/HeroExperience.jsx'
 const Hero = () => {
     return (
         <section id="hero" className='relative overflow-hidden'>
@@ -12,15 +14,15 @@ const Hero = () => {
                 <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
                     <div className='flex flex-col gap-7'>
                         <div className='hero-text'>
-                            <h1>Shaping 
+                            <h1>Shaping
                                 <span className='slide'>
                                     <span className='wrapper'>
                                         {words.map((word) => (
                                             <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
-                                                <img 
-                                                src={word.imgPath}
-                                                alt={word.text}
-                                                className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'/>
+                                                <img
+                                                    src={word.imgPath}
+                                                    alt={word.text}
+                                                    className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50' />
 
                                                 <span>{word.text}</span>
                                             </span>
@@ -33,9 +35,27 @@ const Hero = () => {
                             <h1>that Deliver Results</h1>
                         </div>
 
+                        <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>
+                            Hi, I'm Mark,  a developer based in Kenya with passion for developing User Interfaces, Mobile and Web applications. My expertise lies in building responsive and performant web applications with React, and cross-platform mobile applications using React Native.
+                            I'm proficient in JavaScript, TypeScript and back-end technppologies like Node.js, Python, PHP, SQL and various related libraries, tools and Technologies.
+                        </p>
+
+                        {/* Calling Componetns */}
+                        <Button
+                            className="md:w-80 md:h-16 w-60 h-12"
+                            id="button"
+                            text="See my Work" />
+
                     </div>
 
                 </header>
+
+                {/* RIGH 3D MODEL */}
+                <figure>
+                    <div className='hero-3d-layout'>
+                        <HeroExperience />
+                    </div>
+                </figure>
 
 
             </div>
